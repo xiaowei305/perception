67f5df41607e409607a1cf3ffe778abf6ba0af45
xiaowei <deepcourse@163.com>
2022-04-21 23:11:48 +0800

    fix

--- ch3/ipm/ipm.py
+++ ch3/ipm/ipm.py
@@ -49,8 +49,10 @@ def load_camera_params(file):
                   [0, 0, 1]])
 
     # Extrinsic
-    R_veh2cam = np.transpose(rotation_from_euler(roll, pitch, yaw))
-    T_veh2cam = translation_matrix((-x, -y, -z))
+    # 外参是相机相对于车身坐标的偏移，需要转成车身相对于相机的偏移
+    R_cam2veh = rotation_from_euler(roll, pitch, yaw)
+    R_veh2cam = np.transpose(R_cam2veh)
+    T_veh2cam = -translation_matrix((-x, -y, -z))
 
     # Rotate to camera coordinates
     R = np.array([[0., -1., 0., 0.],
@@ -60,7 +62,7 @@ def load_camera_params(file):
     RT = R @ R_veh2cam @ T_veh2cam
     R = RT[:3, :3]
     T = RT[:3, 3:]
-    return K, R, T, z
+    return K, R, T
 
 
 def get_homography(K, R, T):
@@ -70,10 +72,10 @@ def get_homography(K, R, T):
     return H
 
 
-def ipm(coord, K, R, T, h):
+def ipm(coord, K, R, T):
     H = get_homography(K, R, T)
     xyz = H @ coord
-    xyz = xyz / xyz[2, 0] * h  # 归一化之后再乘以高度，使z坐标与高度一致
+    xyz = xyz / xyz[2, 0]  # 归一化坐标
     return xyz[:, 0]
 
 
@@ -97,7 +99,7 @@ if __name__ == '__main__':
     # Retrieve camera parameters
     image = cv2.cvtColor(cv2.imread('stuttgart_01_000000_003715_leftImg8bit.png'), cv2.COLOR_BGR2RGB)
     TARGET_H, TARGET_W = 500, 500
-    K, R, T, camera_h = load_camera_params('camera.json')
+    K, R, T = load_camera_params('camera.json')
 
     # Warp the image
     warped = draw_ipm(image, K, R, T, TARGET_W, TARGET_H)
@@ -108,20 +110,15 @@ if __name__ == '__main__':
     xc = int(x + w * 0.5)
     yc = y + h
     bottom_center = np.array([[xc], [yc], [1.0]])
-    depth, dy, _ = ipm(bottom_center, K, R, T, camera_h)
+    depth, dy, _ = ipm(bottom_center, K, R, T)
     print(depth, dy)
 
     cv2.circle(image, (xc, yc), 3, (255, 0, 0), 3)
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
     cv2.putText(image, f"depth={depth:.3f}", (x, y),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
-
-    # Draw results
-    fig, ax = plt.subplots(1, 2)
-
-    ax[0].imshow(image)
-    ax[0].set_title('Front View')
-    ax[1].imshow(warped)
-    ax[1].set_title('IPM')
+    cv2.imshow("Front View", image)
+    cv2.waitKey(0)
+    plt.imshow(warped)
     plt.tight_layout()
     plt.show()
